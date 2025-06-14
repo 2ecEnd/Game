4358b52cf6591c1e597b0e47f6aeb616bfb05350
lee_alex <leealex.2006@mail.ru>
2025-06-14 16:04:46 +0700

    added generation of circle arena (only code)

--- Assets/Scripts/Arena/ArenaManager.cs
+++ Assets/Scripts/Arena/ArenaManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -7,15 +8,17 @@ public class ArenaManager : MonoBehaviour
     private GameObject arena;
     private const int arenaSize = 16;
 
+    public int flag = 0;
+
     public GameObject chunk;
     public GameObject stairs441;
-    public GameObject stairs441_concave; // Вогнутая
-    public GameObject stairs441_convex; // Выпуклый
+    public GameObject stairs441_concave;    // Вогнутая
+    public GameObject stairs441_convex;     // Выпуклый
 
     private float chunkScale;
     private float chunkHeight;
     private GameObject[,] chunks;
-    private int[,] heightMatrix;
+    public int[,] heightMap;
 
     private int[,] arena1 = new int[arenaSize, arenaSize]
     {
@@ -65,24 +68,24 @@ public class ArenaManager : MonoBehaviour
         chunkHeight = (chunk.transform.localScale.y / 2);   // Нацало координат чанка находится в его центре
                                                             // Поэтому делим высоту пополам
 
-        arena.transform.position = new Vector3(0, 0, 0);
+        arena.transform.position = new Vector3(0, 10, 0);
 
         chunks = new GameObject[arenaSize, arenaSize];
         //heightMatrix = new int[arenaSize, arenaSize]
-        heightMatrix = arena2;
+        heightMap = arena2;
 
         // Установка базовых значений высот
         for (int i = 0; i < chunks.GetLength(0); i++)
             for (int j = 0; j < chunks.GetLength(1); j++)
             {
                 //heightMatrix[i, j] = 0;
-                if (heightMatrix[i, j] == 0)
+                if (heightMap[i, j] == 0)
                 {
                     placeStairs(i, j);
                 }
                 else
                 {
-                    Vector3 position = new Vector3(chunkScale * i, heightMatrix[i, j] - chunkHeight, chunkScale * j);
+                    Vector3 position = new Vector3(chunkScale * i, heightMap[i, j] - chunkHeight, chunkScale * j);
                     chunks[i, j] = Instantiate(chunk, position, Quaternion.identity, arena.transform);
                 }
             }
@@ -91,7 +94,11 @@ public class ArenaManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (flag != 0)
+        {
+            heightMap = new int[arenaSize, arenaSize];
+            generateCircleArena();
+        }
     }
 
     void transformArena()
@@ -104,15 +111,15 @@ public class ArenaManager : MonoBehaviour
         if (x == 0 || z == 0 || x == arenaSize - 1 || z == arenaSize - 1)
             return;
 
-        int up      = heightMatrix[x, z - 1];
-        int right   = heightMatrix[x + 1, z];
-        int down    = heightMatrix[x, z + 1];
-        int left    = heightMatrix[x - 1, z];
+        int up      = heightMap[x, z - 1];
+        int right   = heightMap[x + 1, z];
+        int down    = heightMap[x, z + 1];
+        int left    = heightMap[x - 1, z];
 
-        int up_right    = heightMatrix[x + 1, z - 1];
-        int down_right  = heightMatrix[x + 1, z + 1];
-        int down_left   = heightMatrix[x - 1, z + 1];
-        int up_left     = heightMatrix[x - 1, z - 1];
+        int up_right    = heightMap[x + 1, z - 1];
+        int down_right  = heightMap[x + 1, z + 1];
+        int down_left   = heightMap[x - 1, z + 1];
+        int up_left     = heightMap[x - 1, z - 1];
 
         // Проверка на прямые лестницы
         if (up == 0 && down == 0 ||
@@ -192,6 +199,45 @@ public class ArenaManager : MonoBehaviour
                     Vector3 position = new Vector3(chunkScale * x, up_right, chunkScale * z);
                     Instantiate(stairs441_convex, position, Quaternion.Euler(0, 90, 0), arena.transform);
                 }
+            }
+        }
+    }
+
+    void generateCircleArena()
+    {
+        int size = arenaSize / 2;
+        int[] tmpHeightMap = new int[size];
+
+        tmpHeightMap[0] = Random.Range(1, 20);
+        for (int i = 1; i < size; i++)
+        {
+            int height = Random.Range(-4, 4);
+            if (height == 0)
+            {
+                tmpHeightMap[i] = tmpHeightMap[i - 1];
+                continue;
+            }
+
+            if (i + 1 == tmpHeightMap.Length)
+            {
+                tmpHeightMap[i] = tmpHeightMap[i - 1]; 
+            }
+            else
+            {
+                tmpHeightMap[i] = 0;
+                tmpHeightMap[i + 1] = tmpHeightMap[i - 1] + height;
+                i++;
+            }
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = i; j < arenaSize - i; j++) 
+            {
+                heightMap[i, j] = tmpHeightMap[i];
+                heightMap[arenaSize - i - 1, j] = tmpHeightMap[i];
+                heightMap[j, i] = tmpHeightMap[i];
+                heightMap[j, arenaSize - i - 1] = tmpHeightMap[i];
             }
         }
     }
